37ac84ad863cea925ecc611c7973a72156690027
Christopher Klapp <christopher@klapp.name>
2009-12-29 16:21:48 -0500

    changed staging environment config to mimick development

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,13 +1,26 @@
-# This is just used for performance testing
-config.cache_classes = true
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+# Settings specified here will take precedence over those in config/environment.rb
 
-config.log_level = :debug
+# In the development environment your application's code is reloaded on
+# every request.  This slows down response time but is perfect for development
+# since you don't have to restart the webserver when you make code changes.
+config.cache_classes = false
 
-config.action_controller.session = { :session_key => "_myapp_session", :secret => "f83596f6af70839845325108d3d4f42df4c64a605a4d805ecb636ba4dc42d41b1f7b179d47aaf1c3a4993f0941908b0d7c6e8d214578a0d9b77a30a9a8657ed5" }
+# Log error messages when you accidentally call methods on nil.
+config.whiny_nils = true
 
-# prevents rails 2.1 from complaining about protect_from_forgery while profiling
-config.action_controller.allow_forgery_protection = false 
+# Enable the breakpoint server that script/breakpointer connects to
+#config.breakpoint_server = true
 
+# Show full error reports and disable caching
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
+
+# DEPRECATED
+# config.action_view.cache_template_extensions         = false
+config.action_view.debug_rjs                         = true
+
+# Don't send emails in development mode
+config.action_mailer.delivery_method = :test
+
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
